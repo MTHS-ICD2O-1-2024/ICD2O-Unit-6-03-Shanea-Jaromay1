b9622f5d3125e705dc508ff081cbc35a41a12d86
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-05-23 17:29:48 +0000

    please I wanna finish dizz

--- js/script.js
+++ js/script.js
@@ -3,3 +3,25 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function gets the Cat fact.
+ * The "async" is there because it will take time for the internet to return the value
+ */
+async function getCatFact() {
+  // the "try" is here because the internet may not be working
+  // it is like an "if ... else" statement"
+  try {
+    const resultJSON = await fetch("https://meowfacts.herokuapp.com/")
+    const jsonData = await resultJSON.json()
+    console.log(jsonData)
+    const randomFactNumber = jsonData.data[0]
+
+    // output
+    document.getElementById("cat-fact").innerHTML = "<p>" + randomFactNumber + "</p>"
+  } catch (error) {
+    console.error(error)
+  }
+}
